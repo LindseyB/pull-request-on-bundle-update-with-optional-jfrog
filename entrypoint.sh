9545c8c478ed829aaa840631a3552f33ec007aa1
Lindsey Bieda <hazardous.narayan@gmail.com>
2020-08-11 10:07:09 -0400

    This is getting very customized and is useful to no one but me

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,6 +33,10 @@ fi
 gem install bundler-diff
 
 bundle config --local build.mysql2 "--with-ldflags=-L/usr/local/opt/openssl/lib"
+bundle config --local build.nokogiri "--with-xml2-dir=/usr --with-xslt-dir=/opt/local --with-iconv-dir=/opt/local"
+
+sudo apt-get -y install libpq-dev
+sudo apt-get -y install postgresql
 
 if [[ -n "$INPUT_JFROG_PATH" ]]; then
   gem update --system 3.1.1 > /dev/null
